4eb09c4753e3780f5ed4d9b0fee719d9197283f2
Rasoulinia <scorpion_rn@yahoo.com>
2023-03-12 01:33:28 +0330

    Base Kernel Types

---
+++ include/pspkerneltypes.pp
@@ -0,0 +1,31 @@
+unit pspkerneltypes;
+
+interface
+
+{$ifndef PSPKERNELTYPES_H}
+{$define PSPKERNELTYPES_H}
+
+uses
+  psptypes;
+
+(** UIDs are used to describe many different kernel objects. *)
+type
+  SceUID = int32;
+
+(* Misc. kernel types. *)
+  SceSize  = uint32;
+  SceSSize = int32;
+
+  SceUChar = uint8;
+  SceUInt  = uint32;
+
+(* File I/O types. *)
+  SceMode  = int32;
+  SceOff   = SceInt64;
+  SceIores = SceInt64;
+
+{$endif}
+
+implementation
+
+end.

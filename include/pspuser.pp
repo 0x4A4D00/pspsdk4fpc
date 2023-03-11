4a3ae211889fe13469a454b0121a559e876eded1
Rasoulinia <scorpion_rn@yahoo.com>
2023-03-12 01:32:43 +0330

    PSP User Base

---
+++ include/pspuser.pp
@@ -0,0 +1,30 @@
+unit pspuser;
+
+interface
+
+
+{$ifndef PSPUSER_H}
+{$define PSPUSER_H}
+
+uses
+  psptypes,
+  pspkerneltypes;
+
+//#include <psptypes.h>
+//#include <pspkerneltypes.h>
+//#include <pspiofilemgr.h>
+//#include <psploadexec.h>
+//#include <pspmoduleinfo.h>
+//#include <pspmodulemgr.h>
+//#include <pspsysmem.h>
+//#include <pspthreadman.h>
+//#include <psputils.h>
+//#include <pspkerror.h>
+//#include <pspstdio.h>
+//#include <pspintrman.h>  
+  
+{$endif}
+
+implementation
+
+end.

c536bd02c99b92a717a2c680bb22502f48d3574d
0x4A4D00 <40355105+0x4A4D00@users.noreply.github.com>
2023-03-13 23:57:27 +0330

    Update pspiofilemgr.pp

--- include/pspiofilemgr.pp
+++ include/pspiofilemgr.pp
@@ -22,43 +22,43 @@ type
 
 {$endif}
 
-function sceIoOpen(const filename: pchar; flags: int32; mode: SceMode): SceUID; external;
+function sceIoOpen(const filename: pchar; flags: int32; mode: SceMode): SceUID; external; cdecl;
 
-function sceIoOpenAsync(const filename: pchar; flags: int32; mode: SceMode): SceUID; external;
+function sceIoOpenAsync(const filename: pchar; flags: int32; mode: SceMode): SceUID; external; cdecl;
 
-function sceIoClose(fd: SceUID): int32; external;
+function sceIoClose(fd: SceUID): int32; external; cdecl;
 
-function sceIoCloseAsync(fd: SceUID): int32; external;
+function sceIoCloseAsync(fd: SceUID): int32; external; cdecl;
 
-function sceIoRead(fd: SceUID; data: pointer; size: SceSize): int32; external;
+function sceIoRead(fd: SceUID; data: pointer; size: SceSize): int32; external; cdecl;
 
-function sceIoReadAsync(fd: SceUID; data: pointer; size: SceSize): int32; external;
+function sceIoReadAsync(fd: SceUID; data: pointer; size: SceSize): int32; external; cdecl;
 
-function sceIoWrite(fd: SceUID; const data: pointer; size: SceSize): int32; external;
+function sceIoWrite(fd: SceUID; const data: pointer; size: SceSize): int32; external; cdecl;
 
-function sceIoWriteAsync(fd: SceUID; const data: pointer; size: SceSize): int32; external;
+function sceIoWriteAsync(fd: SceUID; const data: pointer; size: SceSize): int32; external; cdecl;
 
-function sceIoLseek(fd: SceUID; offset: SceOff; whence: int32): SceOff; external;
+function sceIoLseek(fd: SceUID; offset: SceOff; whence: int32): SceOff; external; cdecl;
 
-function sceIoLseekAsync(fd: SceUID; offset: SceOff; whence: int32): SceOff; external;
+function sceIoLseekAsync(fd: SceUID; offset: SceOff; whence: int32): SceOff; external; cdecl;
 
-function sceIoLseek32(fd: SceUID; offset: int32; whence: int32): int32; external;
+function sceIoLseek32(fd: SceUID; offset: int32; whence: int32): int32; external; cdecl;
 
-function sceIoLseek32Async(fd: SceUID; offset: int32; whence: int32): int32; external; 
+function sceIoLseek32Async(fd: SceUID; offset: int32; whence: int32): int32; external;  cdecl;
 
-function sceIoRemove(const fileio: pchar): int32; external;
+function sceIoRemove(const fileio: pchar): int32; external; cdecl;
 
-function sceIoMkdir(const dir: pchar; mode: SceMode): int32; external;
+function sceIoMkdir(const dir: pchar; mode: SceMode): int32; external; cdecl;
 
-function sceIoRmdir(const path: pchar): int32; external;
+function sceIoRmdir(const path: pchar): int32; external; cdecl;
 
-function sceIoChdir(const path: pchar): int32; external;
+function sceIoChdir(const path: pchar): int32; external; cdecl;
 
-function sceIoRename(const oldname: pchar; const newname: pchar): int32; external;
+function sceIoRename(const oldname: pchar; const newname: pchar): int32; external; cdecl;
 
-function sceIoDopen(const dirname: pchar): SceUID; external;
+function sceIoDopen(const dirname: pchar): SceUID; external; cdecl;
 
-function sceIoDread(fd: SceUID; dir : PSceIoDirent): int32; external;
+function sceIoDread(fd: SceUID; dir : PSceIoDirent): int32; external; cdecl;
 
 
 implementation

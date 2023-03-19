f2554d3b82cc595b8e156c6f02a4b0b46ce8b620
Rasoulinia <scorpion_rn@yahoo.com>
2023-03-19 20:10:16 +0330

    PSP Module Info Procedures Fixed

--- include/pspmoduleinfo.pp
+++ include/pspmoduleinfo.pp
@@ -1,5 +1,9 @@
 unit pspmoduleinfo;
 
+interface
+
+{$Inline on}
+{$Macro  on}
 
 {$ifndef PSPMODULEINFO_H}
 {$define PSPMODULEINFO_H}
@@ -28,25 +32,36 @@ type
 	PSP_MODULE_KERNEL		= $1000
   );
   
-const
+
   SceModuleInfo = _sceModuleInfo;
   
 var
   _gp : array of char; external;
 
+  sce_newlib_priority : uint32;
+  sce_newlib_stack_kb_size : uint32;
+  sce_newlib_attribute : uint32;
+  sce_newlib_nocreate_thread_in_start : int32;
+  sce_newlib_heap_kb_size : int32;
+  sce_newlib_main_thread_name : pchar;
+
+implementation  
+
+
+(**
 procedure PSP_MODULE_INFO(name: pchar; attributes, major_version, minor_version: int16); assembler;
 const
   mname = name;
 var
-  __lib_ent_top, __lib_ent_bottom   : array of char; external;
-  __lib_stub_top, __lib_stub_bottom : array of char; external;
+  __lib_ent_top, __lib_ent_bottom   : array of char;
+  __lib_stub_top, __lib_stub_bottom : array of char;
 asm
 
-    .set push\n"                                               
-    .section .lib.ent.top, \"a\", @progbits\n"                   
-    .align 2\n"                                                  
-    .word 0\n"                                                 
-__lib_ent_top:\n"                                              
+    .set push
+	.section .lib.ent.top, "a", @progbits
+	.align 2
+    .word 0                                                 
+__lib_ent_top:                                              
     .section .lib.ent.btm, \"a\", @progbits\n"                 
     .align 2\n"                                                
 __lib_ent_bottom:\n"                                           
@@ -63,65 +78,24 @@ __lib_stub_bottom:\n"
     .text\n"													
 
 end;
-
-
-
-
-
-
-(*
-($define) PSP_MODULE_INFO(name, attributes, major_version, minor_version) \
-	__asm__ (                                                       \
-	"    .set push\n"                                               \
-	"    .section .lib.ent.top, \"a\", @progbits\n"                 \
-	"    .align 2\n"                                                \
-	"    .word 0\n"                                                 \
-	"__lib_ent_top:\n"                                              \
-	"    .section .lib.ent.btm, \"a\", @progbits\n"                 \
-	"    .align 2\n"                                                \
-	"__lib_ent_bottom:\n"                                           \
-	"    .word 0\n"                                                 \
-	"    .section .lib.stub.top, \"a\", @progbits\n"                \
-	"    .align 2\n"                                                \
-	"    .word 0\n"                                                 \
-	"__lib_stub_top:\n"                                             \
-	"    .section .lib.stub.btm, \"a\", @progbits\n"                \
-	"    .align 2\n"                                                \
-	"__lib_stub_bottom:\n"                                          \
-	"    .word 0\n"                                                 \
-	"    .set pop\n"                                                \
-	"    .text\n"													\
-	);                                                              \
-	extern char __lib_ent_top[], __lib_ent_bottom[];                \
-	extern char __lib_stub_top[], __lib_stub_bottom[];              \
-	SceModuleInfo module_info                                       \
-		__attribute__((section(".rodata.sceModuleInfo"),        \
-			       aligned(16), unused)) = {                \
-	  attributes, { minor_version, major_version }, name, 0, _gp,  \
-	  __lib_ent_top, __lib_ent_bottom,                              \
-	  __lib_stub_top, __lib_stub_bottom                             \
-	}
 *)
 
-procedure PSP_MAIN_THREAD_PRIORITY(priority: uint32); 
-var
-  sce_newlib_priority : uint32 = priority;
-begin
 
+
+
+procedure PSP_MAIN_THREAD_PRIORITY(priority: uint32); inline;
+begin
+  sce_newlib_priority := priority;
 end;
 
-procedure PSP_MAIN_THREAD_STACK_SIZE_KB(size_kb: uint32);
-var
-  sce_newlib_stack_kb_size : uint32 = size_kb;
+procedure PSP_MAIN_THREAD_STACK_SIZE_KB(size_kb: uint32); inline;
 begin
-
+  sce_newlib_stack_kb_size := size_kb;
 end;
 
-procedure PSP_MAIN_THREAD_ATTR(attr: uint32);
-var
-  sce_newlib_attribute : uint32 = attr;
+procedure PSP_MAIN_THREAD_ATTR(attr: uint32); inline;
 begin
-
+  sce_newlib_attribute := attr;
 end;
 
 {$define PSP_MAIN_THREAD_ATTRIBUTE := PSP_MAIN_THREAD_ATTR}
@@ -133,55 +107,23 @@ begin
   PSP_MAIN_THREAD_ATTR(attr);
 end;
 
-procedure PSP_NO_CREATE_MAIN_THREAD;
-var
-  sce_newlib_nocreate_thread_in_start : int32 = 1;
+procedure PSP_NO_CREATE_MAIN_THREAD; inline;
 begin
-
+  sce_newlib_nocreate_thread_in_start := 1;
 end;
 
-procedure PSP_HEAP_SIZE_KB(size_kb: int32);
-var
-  sce_newlib_heap_kb_size : int32 = size_kb;
+procedure PSP_HEAP_SIZE_KB(size_kb: int32); inline;
 begin
-
+  sce_newlib_heap_kb_size := size_kb;
 end;
 
 {$define PSP_HEAP_SIZE_MAX := PSP_HEAP_SIZE_KB(-1)}
 
+procedure PSP_MAIN_THREAD_NAME(const s: pchar); inline;
+begin
+  sce_newlib_main_thread_name := s;
+end;
 
+{$endif}
 
-
-/* Define the main thread's initial priority. */
-#define PSP_MAIN_THREAD_PRIORITY(priority) \
-	unsigned int sce_newlib_priority = (priority)
-/* Define the main thread's stack size (in KB). */
-#define PSP_MAIN_THREAD_STACK_SIZE_KB(size_kb) \
-	unsigned int sce_newlib_stack_kb_size = (size_kb)
-/* Define the main thread's attributes. */
-#define PSP_MAIN_THREAD_ATTR(attr) \
-	unsigned int sce_newlib_attribute = (attr)
-#define PSP_MAIN_THREAD_ATTRIBUTE PSP_MAIN_THREAD_ATTR
-
-/* Define all main thread parameters. */
-#define PSP_MAIN_THREAD_PARAMS(priority, size_kb, attribute) \
-	PSP_MAIN_THREAD_PRIORITY(priority); \
-	PSP_MAIN_THREAD_STACK_SIZE_KB(size_kb); \
-	PSP_MAIN_THREAD_ATTR(attribute)
-
-/* If declared, the runtime code won't create a main thread for the program. */
-#define PSP_NO_CREATE_MAIN_THREAD() \
-	int sce_newlib_nocreate_thread_in_start = 1
-
-/* Declare the size of the heap (in KB) that the program wants to allocate from. */
-#define PSP_HEAP_SIZE_KB(size_kb) \
-	int sce_newlib_heap_kb_size = (size_kb)
-
-/* Declare to allocate maximum heap area */
-#define PSP_HEAP_SIZE_MAX() \
-	PSP_HEAP_SIZE_KB(-1)
-
-/* Declare the name of the main thread */
-#define PSP_MAIN_THREAD_NAME(s) const char* sce_newlib_main_thread_name = (s)
-
-#endif /* PSPMODULEINFO_H */
+end.

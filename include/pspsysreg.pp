61a54c4383c777559079d4b1d333831610e4e0d4
0x4A4D00 <scorpion_rn@yahoo.com>
2023-04-11 01:00:50 +0430

    pspsysreg Unit Added

---
+++ include/pspsysreg.pp
@@ -0,0 +1,25 @@
+unit pspsysreg;
+
+{$ifndef PSPSYSREG_H}
+{$define PSPSYSREG_H}
+
+uses
+  pspkerneltypes;
+
+function sceSysregMeResetEnable: int32; cdecl; external;
+
+function sceSysregMeResetDisable: int32; cdecl; external;
+
+function sceSysregVmeResetEnable: int32; cdecl; external;
+
+function sceSysregVmeResetDisable: int32; cdecl; external;
+
+function sceSysregMeBusClockEnable: int32; cdecl; external;
+
+function sceSysregMeBusClockDisable: int32; cdecl; external;
+
+{$endif}
+
+implementation
+
+end.

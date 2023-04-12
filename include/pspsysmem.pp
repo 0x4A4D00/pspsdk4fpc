003b81a4271d9afb20bc9ce48b6c1be2d379e98f
0x4A4D00 <scorpion_rn@yahoo.com>
2023-04-13 00:22:46 +0430

    pspsysmem Unit Updated

--- include/pspsysmem.pp
+++ include/pspsysmem.pp
@@ -32,15 +32,15 @@ function sceKernelMaxFreeMemSize(): SceSize; cdecl; external;
  * Get the firmware version.
  * 
  * @return The firmware version.
- * 0x01000300 on v1.00 unit,
- * 0x01050001 on v1.50 unit,
- * 0x01050100 on v1.51 unit,
- * 0x01050200 on v1.52 unit,
- * 0x02000010 on v2.00/v2.01 unit,
- * 0x02050010 on v2.50 unit,
- * 0x02060010 on v2.60 unit,
- * 0x02070010 on v2.70 unit,
- * 0x02070110 on v2.71 unit.
+ * $01000300 on v1.00 unit,
+ * $01050001 on v1.50 unit,
+ * $01050100 on v1.51 unit,
+ * $01050200 on v1.52 unit,
+ * $02000010 on v2.00/v2.01 unit,
+ * $02050010 on v2.50 unit,
+ * $02060010 on v2.60 unit,
+ * $02070010 on v2.70 unit,
+ * $02070110 on v2.71 unit.
 *)
 function sceKernelDevkitVersion: int32; cdecl; external;
 
@@ -50,7 +50,7 @@ function sceKernelDevkitVersion: int32; cdecl; external;
 procedure sceKernelPrintf(const format: pchar; args: array of const); cdecl; external;
 (*
 * Also you can use
-* procedure sceKernelPrintf(const format: pchar;); cdecl; external; varargs;
+* procedure sceKernelPrintf(const format: pchar); varargs; cdecl; external;
 *)
 
 {$endif}

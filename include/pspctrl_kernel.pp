c0f9a75972e27678f1eaf6a226685f8cec703e26
0x4A4D00 <scorpion_rn@yahoo.com>
2023-04-17 01:17:08 +0430

    pspctrl_kernel Unit Added

---
+++ include/pspctrl_kernel.pp
@@ -0,0 +1,30 @@
+unit pspctrl_kernel;
+
+interface
+
+{$ifndef __CTRL_KERNEL_H__}
+{$define __CTRL_KERNEL_H__}
+
+type
+  Tcb = function(arg1: int32; arg2: int32; parg: pointer): pointer;
+
+(* 
+ *
+ * mask  - The bits to setup
+ * atype - The type of operation (0 clear, 1 set mask, 2 set button)
+ *)
+procedure sceCtrl_driver_7CA723DC(mask: uint32; atype: uint32); cdecl; external;
+
+function sceCtrl_driver_5E77BC8A(mask: uint32): int32; cdecl; external;
+
+function sceCtrl_driver_5C56C779(no: int32; mask: uint32; cb: Tcb; arg: pointer): int32; cdecl; external;
+
+{$define sceCtrlSetButtonMasks         := sceCtrl_driver_7CA723DC}
+{$define sceCtrlGetButtonMask          := sceCtrl_driver_5E77BC8A}
+{$define sceCtrlRegisterButtonCallback := sceCtrl_driver_5C56C779}
+
+{$endif}
+
+implementation
+
+end.

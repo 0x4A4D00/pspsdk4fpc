853fbb24f5a18d26ac499f67422cd2ee9b3cefe7
0x4A4D00 <scorpion_rn@yahoo.com>
2023-04-24 21:31:02 +0430

    pspaudiolib Unit Added

---
+++ include/pspaudiolib.pp
@@ -0,0 +1,45 @@
+unit pspaudiolib;
+
+interface
+
+{$ifndef __AUDIOLIB_H__}
+{$define __AUDIOLIB_H__}
+
+
+{$define PSP_NUM_AUDIO_CHANNELS := 4}
+{$define PSP_NUM_AUDIO_SAMPLES  := 1024}
+{$define PSP_VOLUME_MAX         := $8000}
+
+type
+  TpspAudioCallback = function(buf: pointer; reqn: uint32; pdata: pointer): pointer;
+  
+  psp_audio_channelinfo = record
+    threadhandle : int32;
+    handle       : int32;
+    volumeleft   : int32;
+    volumeright  : int32;
+    callback     : TpspAudioCallback;
+    pdata        : pointer;
+  end;
+  
+  TpspAudioThreadfunc = function(args: int32; argp: pointer): int32; cdecl; external;
+
+function pspAudioInit: int32; cdecl; external;
+
+procedure pspAudioEndPre; cdecl; external;
+
+procedure pspAudioEnd; cdecl; external;
+
+procedure pspAudioSetVolume(channel: int32; left: int32; right: int32); cdecl; external;
+
+procedure pspAudioChannelThreadCallback(channel: int32; buf: pointer; reqn: uint32); cdecl; external;
+
+procedure pspAudioSetChannelCallback(channel: int32; callback: TpspAudioCallback; pdata: pointer); cdecl; external;
+
+procedure pspAudioOutBlocking(channel: uint32; vol1: uint32; vol2: uint32; buf: pointer); cdecl; external;
+
+{$endif}
+
+implementation
+
+end.

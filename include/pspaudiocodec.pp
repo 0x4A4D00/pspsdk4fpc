0b5c74aef8b2ef54a13a13b854a4473dd2824bbb
0x4A4D00 <scorpion_rn@yahoo.com>
2023-04-28 15:34:52 +0430

    pspaudiocodec Unit Added

---
+++ include/pspaudiocodec.pp
@@ -0,0 +1,22 @@
+unit pspaudiocodec;
+
+interface
+
+{$define PSP_CODEC_AT3PLUS := ($00001000)}
+{$define PSP_CODEC_AT3     := ($00001001)}
+{$define PSP_CODEC_MP3     := ($00001002)}
+{$define PSP_CODEC_AAC     := ($00001003)}
+
+function sceAudiocodecCheckNeedMem(buffer: uint64; atype: int32): int32; cdecl; external;
+
+function sceAudiocodecInit(buffer: uint64; atype: int32): int32; cdecl; external;
+
+function sceAudiocodecDecode(buffer: uint64; atype: int32): int32; cdecl; external;
+
+function sceAudiocodecGetEDRAM(buffer: uint64; atype: int32): int32; cdecl; external;
+
+function sceAudiocodecReleaseEDRAM(buffer: uint64): int32; cdecl; external;
+
+implementation
+
+end.

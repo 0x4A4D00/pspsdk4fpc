8b02aac442bb29501164f80e5e315890a94487e0
0x4A4D00 <scorpion_rn@yahoo.com>
2023-05-11 11:54:38 +0430

    PSP Net Unit Added

---
+++ include/pspnet.pp
@@ -0,0 +1,18 @@
+unit pspnet;
+
+interface
+
+{$ifndef PSPNET_H}
+{$define PSPNET_H}
+
+function sceNetInit(unk1: int32; unk2: int32; unk3: int32; unk4: int32; unk5: int32): int32; cdecl; external;
+
+function sceNetTerm: int32; cdecl; external;
+
+function sceNetEtherNtostr(mac: Puint8: name: Pchar): int32; cdecl; external;
+
+{$endif}
+
+implementation
+
+end.

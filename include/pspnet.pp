f7d27da43a540e5f9928647a142eca0ec7115e18
0x4A4D00 <scorpion_rn@yahoo.com>
2023-05-12 12:16:14 +0430

    PSP Net Unit Fixed

--- include/pspnet.pp
+++ include/pspnet.pp
@@ -9,7 +9,7 @@ function sceNetInit(unk1: int32; unk2: int32; unk3: int32; unk4: int32; unk5: in
 
 function sceNetTerm: int32; cdecl; external;
 
-function sceNetEtherNtostr(mac: Puint8: name: Pchar): int32; cdecl; external;
+function sceNetEtherNtostr(mac: Puint8; name: Pchar): int32; cdecl; external;
 
 {$endif}
 

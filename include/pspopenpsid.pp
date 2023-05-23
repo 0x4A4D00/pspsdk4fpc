9ac2b54a9e0f48ccbb0373566e84fdc6ca5d69cc
0x4A4D00 <scorpion_rn@yahoo.com>
2023-05-23 23:06:22 +0330

    PSP Open PSID Unit Added

---
+++ include/pspopenpsid.pp
@@ -0,0 +1,20 @@
+unit pspopenpsid;
+
+interface
+ 
+{$ifndef __PSPOPENPSID_H__}
+{$define __PSPOPENPSID_H__}
+
+type
+  PpspOpenPSID = ^PspOpenPSID;
+  PspOpenPSID  = record
+    data : array[0..15] of char;
+  end;
+
+function sceOpenPSIDGetOpenPSID(openpsid: PpspOpenPSID): integer; cdecl; external;
+
+{$endif}
+
+implementation
+
+end.

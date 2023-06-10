44a85fada0de4c8b42ecf4c789f09d1f87161cc3
0x4A4D00 <scorpion_rn@yahoo.com>
2023-06-10 23:47:43 +0330

    PSP System Event Unit Added

---
+++ include/pspsysevent.pp
@@ -0,0 +1,39 @@
+unit pspsysevent;
+
+interface
+
+{$ifndef __PSPSYSEVENT_H__}
+{$define __PSPSYSEVENT_H__}
+ 
+type
+  THandler = function(ev_id: int32; ev_name: Pchar; param: pointer; result: Pinteger): Pinteger;
+  
+  PspSysEventHandlerFunc = function(ev_id: int32; ev_name: Pchar; param: pointer; result: Pinteger): Pinteger;
+  
+  PpspSysEventHandler = ^PspSysEventHandler;
+  PspSysEventHandler  = record
+    size      : int32;
+    name      : Pchar;
+    type_mask : int32;
+    handler   : THandler;
+    r28       : int32;
+    busy      : int32;
+    next      : PspSysEventHandler;
+    reeserved : array[0..8] of int32;
+  end;
+ 
+function sceKernelSysEventDispatch(ev_type_mask: int32; ev_id: int32; ev_name: Pchar; param: pointer; result: Pinteger; break_nonzero: int32; break_handler: PpspSysEventHandler): integer; cdecl; external;
+ 
+function sceKernelReferSysEventHandler: PpspSysEventHandler; cdecl; external;
+ 
+function sceKernelIsRegisterSysEventHandler(handler: PpspSysEventHandler): integer; cdecl; external;
+ 
+function sceKernelRegisterSysEventHandler(handler: PpspSysEventHandler): integer; cdecl; external;
+ 
+function sceKernelUnregisterSysEventHandler(handler: PpspSysEventHandler): integer; cdecl; external;
+ 
+{$endif}
+
+implementation
+
+end.
